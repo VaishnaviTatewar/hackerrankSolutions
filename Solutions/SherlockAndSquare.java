bdbbb358a494029e5ec8d7f9683cad438b17aff4
VAISHNAVI, SHASHANK <vaishnavi-shashank@nissanmotor.com>
2019-08-15 22:54:02 +0530

    Sherlock And Squares Solution

---
+++ Solutions/SherlockAndSquare.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SherlockAndSquare {
+
+    static int squares(int a, int b) {
+     
+       int val1 = (int)Math.sqrt(a);
+       int val2 = (int)Math.sqrt(b);
+       int count = val2-val1;
+       if((double)val1 == Math.sqrt(a)) count++;
+       return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] ab = scanner.nextLine().split(" ");
+
+            int a = Integer.parseInt(ab[0]);
+
+            int b = Integer.parseInt(ab[1]);
+
+            int result = squares(a, b);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
